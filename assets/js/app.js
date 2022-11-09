b2da87f35e4e6baeff846232e3320d9a85863aca
alimarchi <marchi_alice@yahoo.com>
2022-11-09 20:37:08 +0100

    mobile/tablet menu ready signup/login in progress

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,6 +2,8 @@ window.addEventListener("load", () => {
     let sidebar = document.querySelector("#sidebar");
     let close = document.querySelector(".close");
     let menu = document.querySelector("#square");
+    let closeModal = document.querySelector(".close-modal");
+    let signUp = document.querySelector("#sign-up");
 
     close.addEventListener("click", () => {
         sidebar.style.width = "0";
@@ -9,5 +11,9 @@ window.addEventListener("load", () => {
 
     menu.addEventListener("click", () => {
         sidebar.style.width = "65%";
+    })
+
+    closeModal.addEventListener("click", () => {
+        signUp.style.display = "none"; 
     })
 });
