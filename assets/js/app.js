d852bb05b34f8373d5caad4755922c883caaaa76
DaniSanchezG <laughingoutloud90@gmail.com>
2022-11-10 18:28:53 +0100

    Help in players save 5 js scroll full

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,40 +1,50 @@
 window.addEventListener("load", () => {
+  // Header
+  let sidebar = document.querySelector("#sidebar");
+  let close = document.querySelector(".close");
+  let menu = document.querySelector("#square");
+  let closeModal = document.querySelector(".close-modal");
+  let signUp = document.querySelector("#sign-up");
 
-    // Header
-    let sidebar = document.querySelector("#sidebar");
-    let close = document.querySelector(".close");
-    let menu = document.querySelector("#square");
-    let closeModal = document.querySelector(".close-modal");
-    let signUp = document.querySelector("#sign-up");
+  close.addEventListener("click", () => {
+    sidebar.style.width = "0";
+  });
 
-    close.addEventListener("click", () => {
-        sidebar.style.width = "0";
-    })
+  menu.addEventListener("click", () => {
+    sidebar.style.width = "65%";
+  });
 
-    menu.addEventListener("click", () => {
-        sidebar.style.width = "65%";
-    })
+  closeModal.addEventListener("click", () => {
+    signUp.style.display = "none";
+  });
 
-    closeModal.addEventListener("click", () => {
-        signUp.style.display = "none"; 
-    })
+  // Slideshow
 
-    // Slideshow
+  let sliderPoints = document.querySelectorAll(".slideChangePoint");
+  let imgToChange = document.querySelector(".imgSlideShow");
+  let imgCounter = 1;
+  for (let i = 0; i < sliderPoints.length; i++) {
+    sliderPoints[i].addEventListener("click", () => {
+      imgCounter = i + 1;
+      imgToChange.classList.toggle("scpopacity");
+      setTimeout(() => {
+        imgToChange.src = `assets/img/slider/${imgCounter}.jpg`;
+      }, 500);
+      setTimeout(() => {
+        imgToChange.classList.toggle("scpopacity");
+      }, 520);
+    });
+  }
+  let scroll = 0;
 
-    let sliderPoints = document.querySelectorAll(".slideChangePoint");
-    let imgToChange = document.querySelector(".imgSlideShow");
-    let imgCounter = 1;
-    for (let i = 0; i < sliderPoints.length; i++) {
-        sliderPoints[i].addEventListener("click", () => {
-            imgCounter = i + 1;
-            imgToChange.classList.toggle("scpopacity");
-            // imgToChange.src = `assets/img/slider/${imgCounter}.jpg`;
-            setTimeout(() => {
-                imgToChange.src = `assets/img/slider/${imgCounter}.jpg`;
-            }, 500);
-            setTimeout(() => {
-                imgToChange.classList.toggle("scpopacity");
-            }, 520);
-        });
-    };
+  // Show menu on scroll up
+  document.addEventListener("scroll", () => {
+    console.log(window.scrollY);
+    if (scroll >= window.scrollY) {
+      document.querySelector("#header").style.position = "fixed";
+    } else {
+      document.querySelector("#header").style.position = "absolute";
+    }
+    scroll = window.scrollY - 1;
+  });
 });
